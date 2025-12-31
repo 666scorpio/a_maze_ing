d750dfeac19725ca8a36c21dc7ab130181c93e74
Amine El Hafidi <aminel-h@c3r8p9.1337.ma>
2025-12-31 18:55:37 +0100

    fucking commit

---
+++ parsing.py
@@ -0,0 +1,64 @@
+def value_supposed(key, value, line):
+    if key == "WIDTH":
+        try:
+            n = int(value)
+        except ValueError:
+            raise ValueError("the WIDTH must be number" + line)
+        if n <= 0:
+                raise ValueError("the WIDTH must be more than 0"
+                                 " and not negative")        
+    if key == "HEIGHT":
+        try:
+            n = int(value)
+        except ValueError:
+            raise ValueError("the HEIGHT must be number" + line)
+        if n <= 0:
+            raise ValueError("the HEIGHT must be more than 0"
+                             " and not negative")
+    if key == "ENTRY" or key == "EXIT":
+        try:
+            x, y = tuple(map(int, value.split(",")))
+        except ValueError:
+            raise ValueError(f"Invalid coordinates: {value}"
+                              " entry or exit must be numbers")
+    if key == "PERFECT" and (not value == "True" and not value == "False"):
+        raise ValueError("the PERFECT must be True or False")
+
+def parse_config(path):
+    try:
+        with open(path) as file:
+            lines = file.readlines()
+            keys_required = [
+                "WIDTH", "HEIGHT", "ENTRY", "EXIT",
+                "OUTPUT_FILE", "PERFECT", "SEED"]
+            config = {}
+            for line in lines:
+                line = line.strip()
+                if not line:
+                    continue
+                if line.startswith("#"):
+                    continue
+                if "=" not in line:
+                    raise ValueError("Invalid config file " + line)
+                key, value = line.split("=", 1)
+                key = key.strip()
+                if key not in keys_required:
+                    raise ValueError("Invalid config file " + line)
+                value = value.strip()
+                value_supposed(key, value, line)
+                if key in ("WIDTH", "HEIGHT", "SEED"):
+                    config[key] = int(value)
+                elif key in ("ENTRY", "EXIT"):
+                    coordinates = tuple(map(int, value.split(",")))
+                    config[key] = coordinates
+                else:
+                    config[key] = value
+            return config
+
+    except Exception as error:
+        print(error)
+        return {}
+
+config = parse_config("file")
+for key, value in config.items():
+    print(key + "=" + str(value))
